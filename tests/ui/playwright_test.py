929759754c6e18010d6f8d92bbf49d5a34361937
Denys Savytskyi <deniska.sav@gmail.com>
2025-10-03 17:36:17 +0300

    changes for playwright

---
+++ tests/ui/playwright_test.py
@@ -0,0 +1,38 @@
+def test_should_create_bug_report(api_request_context: APIRequestContext) -> None:
+    data = {
+        "title": "[Bug] report 1",
+        "body": "Bug description",
+    }
+    new_issue = api_request_context.post(
+        f"/repos/{GITHUB_USER}/{GITHUB_REPO}/issues", data=data
+    )
+    assert new_issue.ok
+
+    issues = api_request_context.get(f"/repos/{GITHUB_USER}/{GITHUB_REPO}/issues")
+    assert issues.ok
+    issues_response = issues.json()
+    issue = list(
+        filter(lambda issue: issue["title"] == "[Bug] report 1", issues_response)
+    )[0]
+    assert issue
+    assert issue["body"] == "Bug description"
+
+
+def test_should_create_feature_request(api_request_context: APIRequestContext) -> None:
+    data = {
+        "title": "[Feature] request 1",
+        "body": "Feature description",
+    }
+    new_issue = api_request_context.post(
+        f"/repos/{GITHUB_USER}/{GITHUB_REPO}/issues", data=data
+    )
+    assert new_issue.ok
+
+    issues = api_request_context.get(f"/repos/{GITHUB_USER}/{GITHUB_REPO}/issues")
+    assert issues.ok
+    issues_response = issues.json()
+    issue = list(
+        filter(lambda issue: issue["title"] == "[Feature] request 1", issues_response)
+    )[0]
+    assert issue
+    assert issue["body"] == "Feature description"

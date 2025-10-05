becd04c89f6b7f9cb9bd7de4d47f684cb65fed7a
Denys Savytskyi <deniska.sav@gmail.com>
2025-10-05 20:19:49 +0300

    changes for playwright: added tests for github api testing

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from modules.api.clients.github import GitHub
 class User:
 
     def __init__(self) -> None:
@@ -22,40 +23,7 @@ def user():
 
     user.remove()
 
-import os
-from typing import Generator
-
-from playwright.sync_api import Playwright, APIRequestContext
-
-GITHUB_API_TOKEN = os.getenv("GITHUB_API_TOKEN")
-assert GITHUB_API_TOKEN, "GITHUB_API_TOKEN is not set"
-
-
-@pytest.fixture(scope="session")
-def api_request_context(
-        playwright: Playwright,
-) -> Generator[APIRequestContext, None, None]:
-    headers = {
-        # We set this header per GitHub guidelines.
-        "Accept": "application/vnd.github.v3+json",
-        # Add authorization token to all requests.
-        # Assuming personal access token available in the environment.
-        "Authorization": f"token {GITHUB_API_TOKEN}",
-    }
-    request_context = playwright.request.new_context(
-        base_url="https://api.github.com", extra_http_headers=headers
-    )
-    yield request_context
-    request_context.dispose()
-
-@pytest.fixture(scope="session", autouse=True)
-def create_test_repository(
-        api_request_context: APIRequestContext,
-) -> Generator[None, None, None]:
-    # Before all
-    new_repo = api_request_context.post("/user/repos", data={"name": GITHUB_REPO})
-    assert new_repo.ok
-    yield
-    # After all
-    deleted_repo = api_request_context.delete(f"/repos/{GITHUB_USER}/{GITHUB_REPO}")
-    assert deleted_repo.ok
+@pytest.fixture
+def github_api():
+    api = GitHub()
+    yield api
--- tests/ui/playwright_test.py
+++ tests/ui/playwright_test.py
@@ -1,6 +1,48 @@
 import pytest
+from enum import auto
+import os
+from typing import Generator
 
-@pytest.mark.playwright
+from playwright.sync_api import Playwright, Page, APIRequestContext, expect
+
+GITHUB_API_TOKEN = os.getenv("GITHUB_API_TOKEN")
+#assert GITHUB_API_TOKEN, "GITHUB_API_TOKEN is not set"
+
+GITHUB_USER = os.getenv("GITHUB_USER")
+#assert GITHUB_USER, "GITHUB_USER is not set"
+
+GITHUB_REPO = "test"
+
+@pytest.fixture(scope="session")
+def api_request_context(
+        playwright: Playwright,
+) -> Generator[APIRequestContext, None, None]:
+    headers = {
+        # We set this header per GitHub guidelines.
+        "Accept": "application/vnd.github.v3+json",
+        # Add authorization token to all requests.
+        # Assuming personal access token available in the environment.
+        "Authorization": f"token {GITHUB_API_TOKEN}",
+    }
+    request_context = playwright.request.new_context(
+        base_url="https://api.github.com", extra_http_headers=headers
+    )
+    yield request_context
+    request_context.dispose()
+
+@pytest.fixture(scope="session", autouse=True)
+def create_test_repository(
+        api_request_context: APIRequestContext,
+) -> Generator[None, None, None]:
+    # Before all
+    new_repo = api_request_context.post("/user/repos", data={"name": GITHUB_REPO})
+    assert new_repo.ok
+    yield
+    # After all
+    deleted_repo = api_request_context.delete(f"/repos/{GITHUB_USER}/{GITHUB_REPO}")
+    assert deleted_repo.ok
+
+#@pytest.mark.playwright
 def test_should_create_bug_report(api_request_context: APIRequestContext) -> None:
     data = {
         "title": "[Bug] report 1",
@@ -20,7 +62,7 @@ def test_should_create_bug_report(api_request_context: APIRequestContext) -> Non
     assert issue
     assert issue["body"] == "Bug description"
 
-@pytest.mark.playwright
+#@pytest.mark.playwright
 def test_should_create_feature_request(api_request_context: APIRequestContext) -> None:
     data = {
         "title": "[Feature] request 1",
